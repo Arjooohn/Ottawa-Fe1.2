db2de81b800c3eaa15edaf345e067d5fc2e8ad02
Qillme <Rmdsabinano@gmail.com>
2024-03-22 21:04:42 +0800

    Sabiñano, Rochelly Miquel D.

--- src/components/pages/Sabinano-Dela Cruz Page/Error.js
+++ src/components/pages/Sabinano-Dela Cruz Page/Error.js
@@ -4,7 +4,7 @@ import "./errorstyles.css"
 function Error() {
   return (
     <div className="content">
-      <h2>Sabiñano, Rochelly Miquel D. - 2021101171</h2>
+      <h2>Sabiñano, Rochelly Miquel D. - 2021101171 </h2>
     </div>
   )
 }
